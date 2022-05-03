3ac3d24f923209a4ed109abe41c93a0e72ddb5b5
Oliver Jagger <ojj23@cam.ac.uk>
2022-05-04 00:33:34 +0100

    Update slice animation

--- slice_animation.m
+++ slice_animation.m
@@ -1,6 +1,8 @@
 clear
 close all
 
+
+
 hfcase = DNS_case('cwl90_window_turb_clean',3);
 %runs =[5];
 %hfcase.readKSlices([],61:85);
@@ -21,5 +23,34 @@ for i=1:hfcase.nSlices
     hfcase.kPlot(slice,'M',ax,[0 1.4],'M')
     %hfcase.jPlot(hfcase.jSlices(i),'tau_w',[],[0 800],'\tau_w')
     exportgraphics(gcf,fullfile(imgfolder,sprintf('img_%03d.png',i)),'Resolution',600);
+    clear slice
+end
+%%
+
+clear
+close all
+
+
+hfcase = DNS_case('cwl90_tripped2',1);
+%runs =[5];
+%hfcase.readKSlices([],61:85);
+imgfolder = fullfile(hfcase.casepath,'run1','animation_images','k_Mach');
+%%
+if ~exist(imgfolder, 'dir')
+       mkdir(imgfolder);
+end
+
+h = figure;
+h.Visible = 'off';
+ax = gca;
+
+%%
+for i=11:hfcase.nSlices
+    fprintf('Plotting slice %d/%d\n',i,hfcase.nSlices)
+    slice = hfcase.readSingleKSlice(i);
+    hfcase.kPlot(slice,'M',ax,[0 1.4],'M')
+    %hfcase.jPlot(hfcase.jSlices(i),'tau_w',[],[0 800],'\tau_w')
+    exportgraphics(gcf,fullfile(imgfolder,sprintf('img_%03d.png',i)),'Resolution',600);
+    clear slice
 end
 %%
